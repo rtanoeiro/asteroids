7f9103ffef72141f8ea61637c6449e3317ca30e7
Ramon Tanoeiro <ramon.tanoeiro@indebted.co>
2024-11-13 09:10:31 +1100

    added move function

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 
@@ -18,10 +18,14 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), width=2)
+        pygame.draw.polygon(screen, "white", self.triangle(), width=2)
 
     def rotate(self, dt):
         self.rotation += dt * PLAYER_TURN_SPEED
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * dt * PLAYER_TURN_SPEED
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -30,3 +34,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
