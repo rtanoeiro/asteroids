0ee791a3b9b2363066985fc45b228877f4953772
Ramon Tanoeiro <ramon.tanoeiro@indebted.co>
2024-11-13 09:03:03 +1100

    added player in main

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ from constants import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
 )
+from player import Player
 
 
 def main():
     pygame.init()
+    delta = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x=x, y=y)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -21,8 +27,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
+        player.update(delta)
         pygame.display.flip()
+
+        delta = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
